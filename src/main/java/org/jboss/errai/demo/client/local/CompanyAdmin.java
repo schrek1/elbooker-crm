047e62b0c997fe0d07b1768239f50677ce7882ad
Ondřej Schrek <ondrej.schrek@gmail.com>
2016-06-21 14:28:12 +0200

    úprava copanyAdmin -> buttony

--- src/main/java/org/jboss/errai/demo/client/local/CompanyAdmin.java
+++ src/main/java/org/jboss/errai/demo/client/local/CompanyAdmin.java
@@ -1,7 +1,9 @@
 package org.jboss.errai.demo.client.local;
 
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.event.shared.GwtEvent;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.ValueListBox;
 import java.io.IOException;
@@ -11,6 +13,7 @@ import org.jboss.errai.ui.nav.client.local.Page;
 import org.jboss.errai.ui.shared.api.annotations.DataField;
 import org.jboss.errai.ui.shared.api.annotations.Templated;
 import com.google.gwt.text.shared.Renderer;
+import com.google.gwt.user.client.ui.Button;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -24,6 +27,7 @@ import org.jboss.errai.demo.client.shared.services.CompanyServices;
 import org.jboss.errai.demo.client.shared.services.UserServices;
 import org.jboss.errai.demo.client.shared.userEntity.Role;
 import org.jboss.errai.demo.client.shared.userEntity.User;
+import org.jboss.errai.ui.shared.api.annotations.EventHandler;
 
 @Page
 @Templated
@@ -73,6 +77,14 @@ public class CompanyAdmin extends Composite{
   });
 
   @Inject
+  @DataField
+  private Button addButton;
+
+  @Inject
+  @DataField
+  private Button removeButton;
+
+  @Inject
   private Caller<CompanyServices> companyCaller;
 
   @Inject
@@ -80,23 +92,39 @@ public class CompanyAdmin extends Composite{
 
   private List<User> companyUsers = new ArrayList<User>();
 
+  @EventHandler("addButton")
+  private void onAddButtonClick(ClickEvent ce){
+    if(this.userBox.isEnabled()){
+      Company target = (Company)this.companyBox.getValue();
+      User addUser = (User)this.userBox.getValue();
+
+      this.accessBox.setValue(addUser);
+
+      this.accessBox.setEnabled(true);
+
+    }
+
+  }
+
+    @EventHandler("removeButton")
+  private void onRemoveButtonClick(ClickEvent ce){
+    if(this.accessBox.isEnabled()){
+      Company target = (Company)this.companyBox.getValue();
+      User removeUser = (User)this.accessBox.getValue();
+
+      this.userBox.setValue(removeUser);
+
+      this.userBox.setEnabled(true);
+
+    }
+
+  }
+
   @PostConstruct
   private void init(){
     this.setUserList();
     this.addHandlerToListBox();
     this.fillCompanyBox();
-  }
-
-  private void fillCompanyBox(){
-    this.companyCaller.call(new RemoteCallback<List<Company>>(){
-      @Override
-      public void callback(List<Company> response){
-        if(!response.isEmpty()){
-          companyBox.setValue(response.get(0), true);
-          companyBox.setAcceptableValues(response);
-        }
-      }
-    }).getListOfCompanies();
   }
 
   private void setUserList(){
@@ -115,32 +143,63 @@ public class CompanyAdmin extends Composite{
         Company selectCompany = (Company)companyBox.getValue();
         List<User> userWithAccess = selectCompany.getAuthorizedUsers();
 
-        if(!userWithAccess.isEmpty()){
-          accessBox.setEnabled(true);
-          accessBox.setValue(userWithAccess.get(0), true);
-          accessBox.setAcceptableValues(userWithAccess);
-        }else{
-          accessBox.setEnabled(false);
-          accessBox.setValue(new User("prazdne",  Arrays.asList(new Role(UsersRole.COMPANY))));
-          accessBox.setAcceptableValues(new ArrayList<User>());
-        }
-
-        List<User> availableUsers = new ArrayList<User>();
-        Collections.copy(availableUsers, companyUsers);
-        availableUsers.removeAll(userWithAccess);
-
-        if(!availableUsers.isEmpty()){
-          userBox.setEnabled(true);
-          userBox.setValue(availableUsers.get(0), true);
-          userBox.setAcceptableValues(availableUsers);
-        }else{
-          userBox.setEnabled(false);
-          userBox.setValue(new User("prazdne",  Arrays.asList(new Role(UsersRole.COMPANY))));
-          userBox.setAcceptableValues(new ArrayList<User>());
-        }
-
+        fillAccessBox(userWithAccess);
+        fillUserBox(userWithAccess);
       }
+
     });
   }
 
+  private void fillAccessBox(List<User> userWithAccess){
+    if(!userWithAccess.isEmpty()){
+      attachToUserBox(accessBox, userWithAccess);
+      this.removeButton.getElement().removeClassName("disabled");
+    }else{
+      setUserBoxEmpty(accessBox);
+      this.removeButton.getElement().addClassName("disabled");
+    }
+  }
+
+  private void fillUserBox(List<User> userWithAccess){
+    List<User> availableUsers = getAvailableUsers(userWithAccess);
+
+    if(!availableUsers.isEmpty()){
+      attachToUserBox(userBox, availableUsers);
+      this.addButton.getElement().removeClassName("disabled");
+    }else{
+      setUserBoxEmpty(userBox);
+      this.addButton.getElement().addClassName("disabled");
+    }
+  }
+
+  private List<User> getAvailableUsers(List<User> userWithAccess){
+    List<User> availableUsers = new ArrayList<User>();
+    Collections.copy(availableUsers, companyUsers);
+    availableUsers.removeAll(userWithAccess);
+    return availableUsers;
+  }
+
+  private void attachToUserBox(ValueListBox listbox, List<User> list){
+    listbox.setEnabled(true);
+    listbox.setValue(list.get(0), true);
+    listbox.setAcceptableValues(list);
+  }
+
+  private void setUserBoxEmpty(ValueListBox listbox){
+    listbox.setEnabled(false);
+    listbox.setValue(new User("prazdne", Arrays.asList(new Role(UsersRole.COMPANY))));
+    listbox.setAcceptableValues(new ArrayList<User>());
+  }
+
+  private void fillCompanyBox(){
+    this.companyCaller.call(new RemoteCallback<List<Company>>(){
+      @Override
+      public void callback(List<Company> response){
+        if(!response.isEmpty()){
+          companyBox.setValue(response.get(0), true);
+          companyBox.setAcceptableValues(response);
+        }
+      }
+    }).getListOfCompanies();
+  }
 }
